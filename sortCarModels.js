16240f733a090c2f97c7d0d7db86a8180b79e270
sukumar <sukumar.sannareddy.30.1@mountblue.tech>
2024-10-05 10:53:48 +0530

    sorting car models

---
+++ sortCarModels.js
@@ -0,0 +1,14 @@
+exports.sortCarModels= function sortCarModels(inventory){
+    try{
+        if(inventory.length==0){
+            return "Inventory is empty"
+        }
+        return inventory.sort((a,b)=>a.car_model.localeCompare(b.car_model))
+    }catch(error){
+        if(error.message.split(' ').includes('undefined')){
+            return "Data is insufficent"
+        }else{
+            return error.message
+        } 
+    }
+};
