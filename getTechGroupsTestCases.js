00017e9af8667b1a51a591309223d829db3e1e4e
sukumar <sukumar.sannareddy.30.1@mountblue.tech>
2024-10-05 12:36:17 +0530

    Grouping based on technologies test cases

---
+++ getTechGroupsTestCases.js
@@ -0,0 +1,47 @@
+const { getTechGroups } = require("./getTechGroups.js");
+
+
+const users = {
+    "John": {
+        age: 24,
+        desgination: "Senior Golang Developer",
+        interests: ["Chess, Reading Comics, Playing Video Games"],
+        qualification: "Masters",
+        nationality: "Greenland"
+    },
+    "Ron": {
+        age: 19,
+        desgination: "Intern - Golang",
+        interests: ["Video Games"],
+        qualification: "Bachelor",
+        nationality: "UK"
+    },
+    "Wanda": {
+        age: 24,
+        desgination: "Intern - Javascript",
+        interests: ["Piano"],
+        qualification: "Bachaelor",
+        nationality: "Germany"
+    },
+    "Rob": {
+        age: 34,
+        desgination: "Senior Javascript Developer",
+        interest: ["Walking his dog, Cooking"],
+        qualification: "Masters",
+        nationality: "USA"
+    },
+    "Pike": {
+        age: 23,
+        desgination: "Python Developer",
+        interests: ["Listing Songs, Watching Movies"],
+        qualification: "Bachaelor's Degree",
+        nationality: "Germany"
+    }
+}
+
+
+
+
+
+console.log(getTechGroups(users));
+console.log(getTechGroups());
