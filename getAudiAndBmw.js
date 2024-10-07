df053f07c79d9980a527b17a64e611f5c990270d
sukumar <sukumar.sannareddy.30.1@mountblue.tech>
2024-10-07 15:10:22 +0530

    get audi and bmw cars

--- getAudiAndBmw.js
+++ getAudiAndBmw.js
@@ -1,20 +1,7 @@
 exports.getAudiAndBmw=function getAudiAndBmw(inventory){
     try{
-        if(inventory.lenght==0){
-            return "inventory is empty"
-        }else{
-            let audiAndBmw=[]
-            for(let i=0;i<inventory.length;i++){
-                if(inventory[i].car_make=='Audi' || inventory[i].car_make=='BMW'){
-                    audiAndBmw.push(inventory[i])
-                }
-            }
-            if(audiAndBmw.length==0){
-                return "No Audi and Bmw cars"
-            }else{
-                return JSON.stringify(audiAndBmw)
-            }
-        }
+        let cars=inventory.filter((object)=> object.car_make=="Audi" ||object.car_make=='BMW')
+        return cars.length==0?"no cars available":JSON.stringify(cars)
     }catch(error){
         if(error.message.split(' ').includes('undefined')){
             return "Data is insufficent"
