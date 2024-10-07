9156a5d2bb4fa575553512adf0404de26025fc9d
sukumar <sukumar.sannareddy.30.1@mountblue.tech>
2024-10-07 15:05:44 +0530

    get cars older than 2000

--- carsOlderThan2000.js
+++ carsOlderThan2000.js
@@ -1,16 +1,7 @@
 exports.CarsOlderThan2000=function CarsOlderThan2000(inventory){
     try{
-        if(inventory.length==0){
-            return "Inevntory is empty"
-        }else{
-            years=[]
-            for(let i=0;i<inventory.length;i++){
-                if(inventory[i].car_year<2000){
-                    years.push(inventory[i].car_year)
-                }
-            }
-            return years.length
-        }
+        let noOfcars=inventory.filter((object)=>object.car_year<2000).length
+        return noOfcars==0?"No car is older than 2000 year":noOfcars
     }catch(error){
         if(error.message.split(' ').includes('undefined')){
             return "Data is insufficent"
