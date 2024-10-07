639054c42dec898cf9429be188dde744cdf6857f
sukumar <sukumar.sannareddy.30.1@mountblue.tech>
2024-10-07 15:12:21 +0530

    get last car

--- getLastCar.js
+++ getLastCar.js
@@ -1,7 +1,11 @@
 exports.getLastCar=function getLastCar(inventory){
     try{
-        let lastCar= inventory[inventory.length-1]
-        return "Last car is a "+lastCar.car_make+","+lastCar.car_model
+        let lastCar=[]
+        if(inventory.length==0){
+            return "inventory is empty"
+        }else{
+             lastCar=inventory[inventory.length-1]
+        }return "Last car is a "+lastCar.car_make+","+lastCar.car_model
     }catch(error){
         if(error.message.split(' ').includes('undefined')){
             return "Data is insufficent"
