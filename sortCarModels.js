588057032e00d7409be22a6e18871fc31781887a
sukumar <sukumar.sannareddy.30.1@mountblue.tech>
2024-10-07 15:13:27 +0530

    sort based on model

--- sortCarModels.js
+++ sortCarModels.js
@@ -2,8 +2,7 @@ exports.sortCarModels= function sortCarModels(inventory){
     try{
         if(inventory.length==0){
             return "Inventory is empty"
-        }
-        return inventory.sort((a,b)=>a.car_model.localeCompare(b.car_model))
+        }return JSON.stringify(inventory.sort((a,b)=>a.car_model.localeCompare(b.car_model)))
     }catch(error){
         if(error.message.split(' ').includes('undefined')){
             return "Data is insufficent"
